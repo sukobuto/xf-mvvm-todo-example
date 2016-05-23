d60314bf425c2e66a276e95063cdc9e0dbd1c581
Kenta Suzuki <k-suzuki@mi-labo.co.jp>
2016-05-23 18:42:53 +0900

    Prism.Forms プロジェクトテンプレートから生成

---
+++ TodoMvvm/TodoMvvm/TodoMvvm/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Unity;
+using TodoMvvm.Views;
+
+namespace TodoMvvm
+{
+    public partial class App : PrismApplication
+    {
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.Navigate("MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
