368987ccaeb88833d6b6c480b4de402f782fa74a
sukobuto <sukobuto@gmail.com>
2016-05-24 14:23:29 +0900

    アイテムの追加と保存まで実装完了

--- TodoMvvm/TodoMvvm/TodoMvvm/ViewModels/MainPageViewModel.cs
+++ TodoMvvm/TodoMvvm/TodoMvvm/ViewModels/MainPageViewModel.cs
@@ -1,23 +1,44 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
+using TodoMvvm.Models;
 
 namespace TodoMvvm.ViewModels
 {
     public class MainPageViewModel : BindableBase, INavigationAware
     {
+        private readonly TodoItemRepository _todoItemRepository = new TodoItemRepository();
+
         private string _title;
         public string Title {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
+        public ReactiveProperty<string> NewItemsText { get; } = new ReactiveProperty<string>();
+
+        public ReactiveCommand AddNewItem { get; private set; }
+
+        public ReactiveCollection<TodoItem> TodoItems { get; } = new ReactiveCollection<TodoItem>();
+
         public MainPageViewModel()
         {
+            this.AddNewItem = this.NewItemsText
+                .Select(x => !string.IsNullOrWhiteSpace(x)) // NewItemsText の状況から bool に変換
+                .ToReactiveCommand();
 
+            this.AddNewItem
+                .Subscribe(async _ => {
+                    var item = new TodoItem { Text = this.NewItemsText.Value, CreatedAt = DateTime.Now, Delete = false };
+                    this.TodoItems.AddOnScheduler(item);
+                    this.NewItemsText.Value = "";
+                    await this._todoItemRepository.SaveItemAsync(item);
+                });
         }
 
         public void OnNavigatedFrom(NavigationParameters parameters)
@@ -25,10 +46,12 @@ namespace TodoMvvm.ViewModels
 
         }
 
-        public void OnNavigatedTo(NavigationParameters parameters)
+        public async void OnNavigatedTo(NavigationParameters parameters)
         {
             if (parameters.ContainsKey("title"))
                 Title = (string)parameters["title"] + " and Prism";
+            // SQLite からデータを取得
+            this.TodoItems.AddRangeOnScheduler(await this._todoItemRepository.GetItemsAsync());
         }
     }
 }
